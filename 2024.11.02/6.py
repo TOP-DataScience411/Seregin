127f628969211f835c47557bf49b89f5b7347596
Seregin <denis.seregin.08@gmail.com>
2024-11-06 19:10:01 +0300

    h/w

--- 2024.11.02/6.py
+++ 2024.11.02/6.py
@@ -3,7 +3,7 @@ import sys
 motion1 = sys.stdin.readline().strip()
 motion2 = sys.stdin.readline().strip()  
 
-if 1 >=(ord(motion1[0]) - ord(motion2[0])) <= 1 and abs(int(motion1[1]) - int(motion2[1])) <= 1:
+if 1 >=(ord(motion1[0]) - ord(motion2[0])) <= 1 and int(motion1[1]) - int(motion2[1]) <= 1:
     print("да")  
 else:
     print("нет") 
