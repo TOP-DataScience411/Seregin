1894e3f5b7509119fab56a9387accdf23d0d9a46
Seregin <denis.seregin.08@gmail.com>
2024-11-06 18:59:06 +0300

    h/w

--- 2024.11.02/2.py
+++ 2024.11.02/2.py
@@ -6,7 +6,7 @@ num2 = int(sys.stdin.readline().strip())
 if num1 % num2 == 0:
     print(f'{num1} делится на {num2} нацело \nчастное: {num1//num2}')
 else:
-    print(f'{num1} не делится на {num2} нацело \nнеполное частное: {num1//num2}\nостаток: {num1%num2%num2}')
+    print(f'{num1} не делится на {num2} нацело \nнеполное частное: {num1//num2}\nостаток: {num1%num2}')
     
 # 8
 # 2
